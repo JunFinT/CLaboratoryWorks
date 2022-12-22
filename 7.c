d693b7d32143027d4c63bce47180b1094cd9af59
Nazar <myolimpmail@gmail.com>
2022-12-22 15:04:14 +0300

    7

--- 7.c
+++ 7.c
@@ -26,205 +26,17 @@
 
 typedef struct Stud
 {
-    char first_name[50], last_name[50];
+    char first_name[100], last_name[100];
     int day, month, year;
     double av_grade;
     struct Stud *next;
 } Stud;
 
-struct command
-{
-    char first[50];
-    char second[50];
-};
+// Вспомогательные функции
 
-int exists(const char *fname);
-void getarg(struct command *com);
-void getstring(char *str);
-void err(char *str);
-void save_db(Stud *st, char *filename);
-void load_db(Stud **st, char name[50]);
-void print_db(Stud *st);
-void add_to_db_helper(Stud **head, char *n, char *sn, int d, int m, int y, double r);
-void add_to_db(Stud **head);
-void delete_from_db(Stud *st, char *sn);
-void search_through_db(Stud *st, char *sn);
-int how_many(Stud *st, char *sn);
-int verify(char *st);
-
-int main(void){
-    printf("Student database\n\nUse commands:\nload (db_name)\nsave (db_name)\n");
-    printf("print\nadd\ndelete (student_surname)\nsearch (student_surname)");
-    printf("\n(warning: all input values must be under 50 symbols!)\n\n");
-    struct Stud *st = NULL;
-
-    struct command str;
-    while(1){
-        strcpy(str.first, "");
-        strcpy(str.second, "");
-        getarg(&str);
-        if(strcmp(str.first, "") == 0){
-            continue;
-        }else if(strcmp(str.first, "load") == 0 && strcmp(str.second, "") != 0){
-            load_db(&st, str.second);
-        }else if(strcmp(str.first, "save") == 0 && strcmp(str.second, "") != 0){
-            save_db(st, str.second);
-        }else if(strcmp(str.first, "print") == 0 && strcmp(str.second, "") == 0){
-            print_db(st);
-        }else if(strcmp(str.first, "add") == 0 && strcmp(str.second, "") == 0){
-            add_to_db(&st);
-        }else if(strcmp(str.first, "delete") == 0 && strcmp(str.second, "") != 0){
-            delete_from_db(st, str.second);
-        }else if(strcmp(str.first, "search") == 0 && strcmp(str.second, "") != 0){
-            search_through_db(st, str.second);
-        }else
-            printf("\nUnknown command or incorrect number of arguments!\n\n");
-    }
-    return 0;
-}
-
-void getarg(struct command *com){
-    char str[50];
-    gets(str);
-    char *first = strtok(str, " ");
-    if(!first){
-        return;
-    }
-    char *second = strtok(NULL, " ");
-    strcpy(com->first, "");
-    strcpy(com->first, first);
-    strcpy(com->second,"");
-    if(second){
-        strcpy(com->second, second);
-    }
-    return;
-};
-
-
-void save_db(struct Stud *st, char *filename){
-    if(st == NULL){
-        printf("\nError! The database you're trying to save is empty!\n\n");
-        return;
-    }
-    char *filen = (char*) malloc((strlen(filename) + strlen(".cdb") + 1) * sizeof(char));
-    strcpy(filen, filename);
-    strcat(filen, ".cdb");
-    if(exists(filen)){
-        if(!verify("\nThis action will rewrite an existing file!\nAre you sure you want to save?")){
-            printf("\nCancelled.\n\n");
-            return;
-        }
-    }
-    FILE *f = fopen(filen, "wb");
-    while(69){
-        if(st == NULL)
-            break;
-        fprintf(f, "----\n");
-        fprintf(f, "%s\n", st->first_name);
-        fprintf(f, "%s\n", st->last_name);
-        fprintf(f, "%d\n", st->year);
-        fprintf(f, "%d\n", st->month);
-        fprintf(f, "%d\n", st->day);
-        fprintf(f, "%f\n", st->av_grade);
-        st = st->next;
-    }
-    fclose(f);
-    printf("\nSaved successfully to %s\n\n", filen);
-    return;
-}
-
-void add_to_db_helper(struct Stud **head, char *fn, char *sn, int d, int m, int y, double r){
-    Stud *st = (Stud*)malloc(sizeof(Stud));
-    strcpy(st->first_name, fn);
-    strcpy(st->last_name, sn);
-    st->day = d;
-    st->month = m;
-    st->year = y;
-    st->av_grade = r;
-    st->next = NULL;
-    if(*head == NULL){
-        *head = st;
-        return;
-    }
-    Stud *h = *head;
-    while(h->next != NULL){
-        h = h->next;
-    }
-    h->next = st;
-}
-
-void add_to_db(Stud **head){
-    char fn[50];
-    char sn[50];
-    int d, m, y;
-    double r;
-    printf("\nPlease enter all information about the student\nFirst name: ");
-    scanf("%s", fn);
-    printf("Last name: ");
-    scanf("%s", sn);
-    printf("Date of birth (in the DD.MM.YYYY format): ");
-    scanf("%d.%d.%d", &d, &m, &y);
-    printf("Average grade: ");
-    scanf("%lf", &r);
-
-    add_to_db_helper(head, fn, sn, d, m, y, r);
-    printf("\nStudent saved successfully.\n\n");
-    return;
-}
-
-int pop(Stud **head) {
-    if (*head == NULL)
-        return 0;
-    Stud* prev = NULL;
-    prev = (*head);
-    (*head) = (*head)->next;
-    free(prev);
-    return 1;
-}
-
-void load_db(Stud **st, char name[50]){
-    strcat(name, ".cdb");
-    if(!exists(name)){
-        printf("\nThe file you're trying to open does not exist!\n\n");
-        return;
-    }
-    if(!verify("\nThis action will erase all the unsaved data!\nAre you sure you want to load?")){
-        printf("\nCancelled.\n\n");
-        return;
-    }
-    while(pop(st)){
-        continue;
-    }
-    FILE *f = fopen(name, "rb");
-    char fn[50], sn[50], temp[50];
-    int d, m, y;
-    double gr;
-    fgets(temp, 50, f);
-    while(1){
-        fscanf(f, "%s", fn);
-        fscanf(f, "%s", sn);
-        fgets(temp, 50, f);
-        fgets(temp, 50, f);
-        d = atoi(temp);
-        fgets(temp, 50, f);
-        m = atoi(temp);
-        fgets(temp, 50, f);
-        y = atoi(temp);
-        fgets(temp, 50, f);
-        gr = atof(temp);
-        add_to_db_helper(st, fn, sn, d, m, y, gr);
-        if(fgets(temp, 50, f) == NULL){
-            break;
-        }
-    }
-    fclose(f);
-    printf("\nFile loaded successfully.\n\n");
-    return;
-}
-
-int exists(const char *fname){
+int exists(const char *filename){
     FILE *file;
-    if ((file = fopen(fname, "rb")))
+    if ((file = fopen(filename, "rb")))
     {
         fclose(file);
         return 1;
@@ -232,100 +44,209 @@ int exists(const char *fname){
     return 0;
 }
 
-void print_db(Stud *st){
-    if(st == NULL){
-        printf("\nThe database is empty!\n\n");
+void creator(struct Stud **head, char *first_name, char *last_name, int day, int month, int year, double average_grade){
+    Stud *student = (Stud*)malloc(sizeof(Stud));
+
+    strcpy(student->first_name, first_name);
+    strcpy(student->last_name, last_name);
+    student->day = day;
+    student->month = month;
+    student->year = year;
+    student->av_grade = average_grade;
+    student->next = NULL;
+
+    if(*head == NULL){
+        *head = student;
+    } else {
+        Stud *head_student = *head;
+        while (head_student->next != NULL) {
+            head_student = head_student->next;
+        }
+        head_student->next = student;
+    }
+}
+
+int counter(Stud *student, char *last_name){
+    int count = 0;
+    while(student != NULL){
+        if(strcmp(student->last_name, last_name) == 0)
+            count++;
+        student = student->next;
+    }
+    return count;
+}
+
+// Основные функции
+
+void save(struct Stud *student, char *filename){
+    char *filename_full = (char*) malloc((strlen(filename) + strlen(".cdb") + 1) * sizeof(char));
+    strcpy(filename_full, filename);
+    strcat(filename_full, ".cdb");
+
+    if(exists(filename_full)){
+        printf("This file is already exists\n");
+        return;
+    }
+
+    FILE *file = fopen(filename_full, "wb");
+
+    while(1){
+        if(student == NULL)
+            break;
+        fprintf(file, "-------------------------\n");
+        fprintf(file, "%s\n", student->first_name);
+        fprintf(file, "%s\n", student->last_name);
+        fprintf(file, "%d\n", student->year);
+        fprintf(file, "%d\n", student->month);
+        fprintf(file, "%d\n", student->day);
+        fprintf(file, "%f\n", student->av_grade);
+        student = student->next;
+    }
+    fclose(file);
+
+}  // enter the name of the database
+
+void load(Stud **student, char filename[100]){
+    strcat(filename, ".cdb");
+
+    if(!exists(filename)){
+        printf("There are no file with this name\n");
+        return;
+    }
+
+    FILE *file = fopen(filename, "rb");
+    char first_name[100], last_name[100], temp[100];
+    int day, month, year;
+    double average_graduate;
+    fgets(temp, 100, file);
+    while(1){
+        fscanf(file, "%s", first_name);
+        fscanf(file, "%s", last_name);
+        fgets(temp, 100, file);
+        fgets(temp, 100, file);
+        day = atoi(temp);
+        fgets(temp, 100, file);
+        month = atoi(temp);
+        fgets(temp, 100, file);
+        year = atoi(temp);
+        fgets(temp, 100, file);
+        average_graduate = atof(temp);
+        creator(student, first_name, last_name, day, month, year, average_graduate);
+        if(fgets(temp, 100, file) == NULL){
+            break;
+        }
+    }
+    fclose(file);
+    printf("\nFile loaded successfully.\n");
+}  // enter the name of the database
+
+void add(Stud **head){
+    char fn[100];
+    char sn[100];
+    int d, m, y;
+    double r;
+    printf(" Enter first name: ");
+    scanf("%s", fn);
+    printf("Enter last name: ");
+    scanf("%s", sn);
+    printf("Enter date of birth (in the DD.MM.YYYY format): ");
+    scanf("%d.%d.%d", &d, &m, &y);
+    printf("Enter average grade: ");
+    scanf("%lf", &r);
+
+    creator(head, fn, sn, d, m, y, r);
+}
+
+void print(Stud *student){
+    if(student == NULL){
+        printf("This database is empty\n");
         return;
     }
     int c = 0;
-    while(st != NULL){
+    while(student != NULL){
         c++;
-        printf("\nStudent #%d:\nName: %s; Surname: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n\n",
-               c, st->first_name, st->last_name, st->day, st->month, st->year, st->av_grade);
-        st = st->next;
+        printf("Student №%d:\nName: %s; last_name: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n---------------\n", c, student->first_name, student->last_name, student->day, student->month, student->year, student->av_grade);
+        student = student->next;
     }
-    return;
 }
 
-void search_through_db(Stud *st, char *sn){
-    printf("\nSearching...\n");
+void search(Stud *student, char *last_name){
     int c = 0, fl = 1;
-    while(st != NULL){
+    while(student != NULL){
         c++;
-        if(strcmp(st->last_name, sn) == 0){
-            printf("\nStudent #%d:\nName: %s; Surname: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n\n",
-                   c, st->first_name, st->last_name, st->day, st->month, st->year, st->av_grade);
+        if(strcmp(student->last_name, last_name) == 0){
+            printf("Student №%d:\nName: %s; last_name: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n", c, student->first_name, student->last_name, student->day, student->month, student->year, student->av_grade);
             fl = 0;
         }
-        st = st->next;
+        student = student->next;
     }
     if(fl)
-        printf("\nNo students found with this surname.\n\n");
-    return;
+        printf("There are no students with this last_name\n");
+}  // enter the last_name of the student
+
+
+
+void delete(Stud *student, char *last_name){
+    Stud *last = student;
+    search(student, last_name);
+
+    while(strcmp(student->last_name, last_name) != 0){
+        last = student;
+        student = student->next;
+    }
+    last->next = student->next;
+    free(student);
 }
 
-int how_many(Stud *st, char *sn){
-    int c = 0;
-    while(st != NULL){
-        if(strcmp(st->last_name, sn) == 0)
-            c++;
-        st = st->next;
-    }
-    return c;
-}
 
-void delete_from_db(Stud *st, char *sn){
-    int c = how_many(st, sn);
-    int choice;
-    if(c == 0){
-        printf("\nNo students found with this surname.\n\n");
-        return;
-    }
-    Stud *prev = st;
-    if(c == 1){
-        search_through_db(st, sn);
-        if(!verify("\nAre you sure you want to delete this student?")){
-            printf("\nCancelled.\n\n");
-            return;
-        }
-        while(strcmp(st->last_name, sn) != 0){
-            prev = st;
-            st = st->next;
-        }
-        prev->next = st->next;
-        free(st);
-        printf("\nStudent deleted.\n\n");
-    }else{
-        printf("\nIt appears there are several students with this surname.\nPlease pick one of them by their number:\n");
-        search_through_db(st, sn);
-        scanf("%d", &choice);
-        if(!verify("\nAre you sure you want to delete this student?")){
-            printf("Cancelled.\n\n");
-            return;
-        }
-        for(int i=0; i<choice - 1; i++){
-            printf("%s\n", st->last_name);
-            prev = st;
-            st = st->next;
-        }
-        prev->next = st->next;
-        free(st);
-        printf("\nStudent deleted.\n\n");
-    }
-    return;
-}
 
-int verify(char *st){
-    printf("%s [y]/[n]\n", st);
-    char ans;
+
+
+int main(void){
+    struct Stud *student = NULL;
+    char input[100];
+
     while(1){
-        scanf("%s", &ans);
-        if(ans == 'n')
-            return 0;
-        else if(ans == 'y')
-            return 1;
-        else
-            printf("[y]/[n]\n");
+
+        gets(input);
+
+
+        if(strcmp(input, "") == 0){
+            continue;
+        }
+        char *command = strtok(input, " ");
+
+        if(strcmp(command, "load") == 0){
+
+            char *name = strtok(NULL, " ");
+            load(&student, name);
+
+        }else if(strcmp(command, "save") == 0){
+            char *filename = strtok(NULL, " ");
+
+            save(student, filename);
+
+        }else if(strcmp(command, "print") == 0){
+
+            print(student);
+
+        }else if(strcmp(command, "add") == 0){
+
+            add(&student);
+
+        }else if(strcmp(command, "delete") == 0){
+
+            char *last_name = strtok(NULL, " ");
+            delete(student, last_name);
+
+        }else if(strcmp(command, "search") == 0){
+
+            char *last_name = strtok(NULL, " ");
+            search(student, last_name);
+
+        }else
+
+            printf("!Nope!\n");
     }
 }
- 
+
--- test.c
+++ test.c
@@ -1,32 +1,280 @@
+/*
+7. Написать программу для создания и использования "базы данных" об успеваемости студентов. Данные об одном студенте должны храниться в структуре следующего вида:
+ struct stud
+{
+   char *first_name, *last_name;
+   int year, month, day;
+   double av_grade;
+   struct stud *next;
+};
+Здесь first_name и last_name - указатели на имя и фамилию студента; year, month, day - год, месяц и день рождения студента,
+ av_grade - текущий средний балл студента, next - указатель на структуру, хранящую информацию о следующем студенте.
+Программа должна предоставлять пользователю следующие возможности:
+1. Считывать исходную базу данных из файла, имя файла должно запрашиваться у пользователя,
+2. Записывать текущую базу данных в файл, имя файла должно запрашиваться у пользователя,
+3. Выводить текущее содержимое базы данных на экран,
+4. Добавлять запись в базу данных как вводя данные с клавиатуры, так и считывая данные из файла, имя файла должно запрашиваться у пользователя,
+5. Удалять произвольные записи из базы данных,
+6. Искать студентов в базе данных по фамилии.
+Все записи базы данных должны быть объединены в связный список.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#define q 0.0000001
+#include <string.h>
+#include <ctype.h>
 
-double fun(double x){
-    return pow(x,3);
-}
-double find_int(double a, double b, double eps, double (* f)(double x)){
-    double sum = 1000000000 ;
-    double sut = -10000000000 ;
-    while (fabs(sum* eps-sut* eps/2) > q){
-        sum = 0;
-        sut = 0;
-        for (double i = a; i < b; i+=eps){
-            sum += f(i) + f(i+eps);
-        }
-        for (double i = a; i < b; i+=eps/2){
-            sut += f(i) + f(i+eps/2);
-        }
-        eps=eps/2;
-    }
-    return sum * eps;
-}
-
-
-int main()
+typedef struct Stud
 {
-    printf("%f", find_int(0, 1, 0.0000001, fun));
+    char first_name[50], last_name[50];
+    int day, month, year;
+    double av_grade;
+    struct Stud *next;
+} Stud;
 
+struct command
+{
+    char first[100];
+    char second[100];
+};
+
+
+void getArguments(struct command *com){
+    char str[50];
+    gets(str);
+    char *first = strtok(str, " ");
+    if(!first){
+        return;
+    }
+    char *second = strtok(NULL, " ");
+    strcpy(com->first, "");
+    strcpy(com->first, first);
+    strcpy(com->second,"");
+    if(second){
+        strcpy(com->second, second);
+    }
+};
+
+int exists(const char *firstname){
+    FILE *file;
+    if ((file = fopen(firstname, "rb")))
+    {
+        fclose(file);
+        return 1;
+    }
     return 0;
 }
+
+void creator(struct Stud **head, char *first_name, char *surname, int day, int month, int year, double average_grade){
+    Stud *st = (Stud*)malloc(sizeof(Stud));
+    strcpy(st->first_name, first_name);
+    strcpy(st->last_name, surname);
+    st->day = day;
+    st->month = month;
+    st->year = year;
+    st->av_grade = average_grade;
+    st->next = NULL;
+    if(*head == NULL){
+        *head = st;
+        return;
+    }
+    Stud *h = *head;
+    while(h->next != NULL){
+        h = h->next;
+    }
+    h->next = st;
+}
+
+int pop(Stud **head) {
+    if (*head == NULL)
+        return 0;
+    Stud* prev = NULL;
+    prev = (*head);
+    (*head) = (*head)->next;
+    free(prev);
+    return 1;
+}
+
+void save(struct Stud *student, char *filename){
+    if(student == NULL){
+        printf("\nError! The database you're trying to save is empty!\n\n");
+        return;
+    }
+    char *filen = (char*) malloc((strlen(filename) + strlen(".cdb") + 1) * sizeof(char));
+    strcpy(filen, filename);
+    strcat(filen, ".cdb");
+    if(exists(filen)){
+        printf("\nCancelled.\n\n");
+        return;
+    }
+    FILE *f = fopen(filen, "wb");
+    while(69){
+        if(student == NULL)
+            break;
+        fprintf(f, "----\n");
+        fprintf(f, "%s\n", student->first_name);
+        fprintf(f, "%s\n", student->last_name);
+        fprintf(f, "%d\n", student->year);
+        fprintf(f, "%d\n", student->month);
+        fprintf(f, "%d\n", student->day);
+        fprintf(f, "%f\n", student->av_grade);
+        student = student->next;
+    }
+    fclose(f);
+    printf("\nSaved successfully to %s\n\n", filen);
+}  // enter the name of the database
+
+void load(Stud **student, char name[50]){
+    strcat(name, ".cdb");
+    if(!exists(name)){
+        printf("\nThe file you're trying to open does not exist!\n\n");
+        return;
+    }
+    while(pop(student)){}
+    FILE *file = fopen(name, "rb");
+    char first_name[50], surname[50], temp[50];
+    int day, month, year;
+    double average_graduate;
+    fgets(temp, 50, file);
+    while(1){
+        fscanf(file, "%s", first_name);
+        fscanf(file, "%s", surname);
+        fgets(temp, 50, file);
+        fgets(temp, 50, file);
+        day = atoi(temp);
+        fgets(temp, 50, file);
+        month = atoi(temp);
+        fgets(temp, 50, file);
+        year = atoi(temp);
+        fgets(temp, 50, file);
+        average_graduate = atof(temp);
+        creator(student, first_name, surname, day, month, year, average_graduate);
+        if(fgets(temp, 50, file) == NULL){
+            break;
+        }
+    }
+    fclose(file);
+    printf("\nFile loaded successfully.\n\n");
+}  // enter the name of the database
+
+void add(Stud **head){
+    char fn[50];
+    char sn[50];
+    int d, m, y;
+    double r;
+    printf("\nPlease enter all information about the student\nFirst name: ");
+    scanf("%s", fn);
+    printf("Last name: ");
+    scanf("%s", sn);
+    printf("Date of birth (in the DD.MM.YYYY format): ");
+    scanf("%d.%d.%d", &d, &m, &y);
+    printf("Average grade: ");
+    scanf("%lf", &r);
+
+    creator(head, fn, sn, d, m, y, r);
+    printf("\nStudent saved successfully.\n\n");
+}
+
+void printDb(Stud *student){
+    if(student == NULL){
+        printf("\nThe database is empty!\n\n");
+        return;
+    }
+    int c = 0;
+    while(student != NULL){
+        c++;
+        printf("\nStudent #%d:\nName: %s; Surname: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n\n",
+               c, student->first_name, student->last_name, student->day, student->month, student->year, student->av_grade);
+        student = student->next;
+    }
+}
+
+void search(Stud *student, char *surname){
+    int c = 0, fl = 1;
+    while(student != NULL){
+        c++;
+        if(strcmp(student->last_name, surname) == 0){
+            printf("Student №%d:\nName: %s; Surname: %s;\nBirth date: %d.%d.%d;\nAverage grade: %f\n\n",
+                   c, student->first_name, student->last_name, student->day, student->month, student->year, student->av_grade);
+            fl = 0;
+        }
+        student = student->next;
+    }
+    if(fl)
+        printf("There are students no with this surname\n");
+}
+
+int counter(Stud *student, char *surname){
+    int c = 0;
+    while(student != NULL){
+        if(strcmp(student->last_name, surname) == 0)
+            c++;
+        student = student->next;
+    }
+    return c;
+}
+
+void delete(Stud *st, char *sn){
+    int c = counter(st, sn);
+    int choice;
+    if(c == 0){
+        printf("\nNo students found with this surname.\n\n");
+        return;
+    }
+    Stud *prev = st;
+    if(c == 1){
+        search(st, sn);
+
+        while(strcmp(st->last_name, sn) != 0){
+            prev = st;
+            st = st->next;
+        }
+        prev->next = st->next;
+        free(st);
+        printf("\nStudent deleted.\n\n");
+    }else{
+        printf("\nIt appears there are several students with this surname.\nPlease pick one of them by their number:\n");
+        search(st, sn);
+        scanf("%d", &choice);
+    }
+    for(int i=0; i<choice - 1; i++){
+        printf("%s\n", st->last_name);
+        prev = st;
+        st = st->next;
+    }
+    prev->next = st->next;
+    free(st);
+    printf("\nStudent deleted.\n\n");
+}
+
+
+
+
+
+int main(void){
+    struct Stud *st = NULL;
+
+    struct command str;
+    while(1){
+        strcpy(str.first, "");
+        strcpy(str.second, "");
+        getArguments(&str);
+        if(strcmp(str.first, "") == 0){
+            continue;
+        }else if(strcmp(str.first, "load") == 0 && strcmp(str.second, "") != 0){
+            load(&st, str.second);
+        }else if(strcmp(str.first, "save") == 0 && strcmp(str.second, "") != 0){
+            save(st, str.second);
+        }else if(strcmp(str.first, "print") == 0 && strcmp(str.second, "") == 0){
+            printDb(st);
+        }else if(strcmp(str.first, "add") == 0 && strcmp(str.second, "") == 0){
+            add(&st);
+        }else if(strcmp(str.first, "delete") == 0 && strcmp(str.second, "") != 0){
+            delete(st, str.second);
+        }else if(strcmp(str.first, "search") == 0 && strcmp(str.second, "") != 0){
+            search(st, str.second);
+        }else
+            printf("\nUnknown command or incorrect number of arguments!\n\n");
+    }
+}
+
